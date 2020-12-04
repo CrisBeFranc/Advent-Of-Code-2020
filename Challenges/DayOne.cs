e77016c5daf74d05cbdd37cc3eac51d0b4f28375
Cristian Benavides Franco <CRISTIAN444@bailliegifford.com>
2020-12-04 00:13:54 +0000

    Colour Palette and Basic Data Gathering

---
+++ Challenges/DayOne.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020.Challenges
+{
+    public class DayOne
+    {
+        public string PartOne()
+        {
+            var numbers = new List<int>();
+            var map = new HashSet<int>();
+            var year = 2020;
+            bool found = false;
+
+            //FileStream fileStream = new FileStream("/DataFeed/DayOne.txt", FileMode.Open);
+            //using (StreamReader reader = new StreamReader(fileStream))
+            //{
+            //    string line;
+            //    while ((line = reader.ReadLine()) != null)
+            //    {
+            //        numbers.Add(Convert.ToInt32(line));
+            //    }
+            //}
+
+
+            return "Hi";
+        }
+
+    }
+}
